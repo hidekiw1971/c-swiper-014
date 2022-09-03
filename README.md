f64fdf330b7841ea75a7405f942ce507b669f0ac
hideki watabe <hideki.w.1971@gmail.com>
2022-09-03 15:32:53 +0900

    2022/9/3 swiper 無限ループ完成

---
+++ README.md
@@ -0,0 +1,9 @@
+# compornent（共通部品）
+
+## portfolio url:
+
+- https://c-0098.wtb.cfbx.jp/
+
+## 更新履歴
+
+- 2022/9/3 swiper 無限ループ完成
---
+++ js/script.js
@@ -0,0 +1,13 @@
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
+
+  const swiper = new Swiper(".swiper", {
+    loop: true, // ループ有効
+    slidesPerView: 3, // 一度に表示する枚数
+    speed: 6000, // ループの時間
+    allowTouchMove: false, // スワイプ無効
+    autoplay: {
+      delay: 0, // 途切れなくループ
+    },
+  });
+});
